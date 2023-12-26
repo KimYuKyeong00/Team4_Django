ffe03c47e54a9cb3204c9a077a5ca96ed218d6a0
kky <fg003331@gmail.com>
2023-12-26 11:47:20 +0900

    요약기능 추가

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import responseToSpring
+from .views import summaryToSpring
+
 urlpatterns = [
     path('responseToSpring',responseToSpring,name='responseToSpring'),
-    
+    path('summaryToSpring',summaryToSpring,name='summaryToSpring'),
 ]
