ee795484ee61e5c89355f76a2f6a593126660041
kky <fg003331@gmail.com>
2023-12-27 14:37:54 +0900

    비동기처리를 통해 응답속도 개선

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -11,10 +11,14 @@ from google.cloud import speech_v1p1beta1 as speech
 from google.protobuf.json_format import MessageToJson
 import json
 from konlpy.tag import Okt
+from konlpy.tag import Kkma
 from collections import Counter
 import torch
 from transformers import PreTrainedTokenizerFast
 from transformers import BartForConditionalGeneration
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import concurrent.futures
 # AudioSegment.converter = "C:/ffmpeg-6.1-essentials_build/ffmpeg-6.1-essentials_build/bin"
 
 
@@ -27,27 +31,49 @@ def makeFolderName():
     foldername = f"folder_{uuid.uuid4()}"
     return foldername
 
+
+
+
 def responseToSpring(request):
     try:
         folder_dir = request.GET.get('folder_dir')
         fileName = request.GET.get('fileName')
+        flac_dir = videoToAudio(folder_dir, fileName)
+        audio_paths = split_flac(flac_dir, folder_dir, 30)
+        result = transcribe_audio_parallel(audio_paths)
+        deleteUsedLocalFiles(flac_dir, audio_paths)
         
-        print(folder_dir)
-        print(fileName)
-        
-        flac_dir = videoToAudio(folder_dir,fileName)
-        gcs_fileName = makeFileName('flac')
-        bn =upload_file_to_gcs(flac_dir,gcs_fileName)
-        flacInGCS = "gs://audio_storage_tpj/"+bn
-        result = transcribe_audio(flacInGCS)
-        deleteUsedLocalFile(flac_dir)
-        deleteUsedGCSFile(gcs_fileName)
+        return JsonResponse(result)
         
     except Exception as e:
         print(str(e))
         return HttpResponse(f"error :{e}")
     
-    return HttpResponse(result,content_type='application/json')
+
+
+# def responseToSpring(request):
+#     try:
+#         folder_dir = request.GET.get('folder_dir')
+#         fileName = request.GET.get('fileName')
+        
+#         print(folder_dir)
+#         print(fileName)
+        
+#         flac_dir = videoToAudio(folder_dir,fileName)
+#         gcs_fileName = makeFileName('flac')
+#         bn =upload_file_to_gcs(flac_dir,gcs_fileName)
+#         flacInGCS = "gs://audio_storage_tpj/"+bn
+#         result = transcribe_audio(flacInGCS)
+#         deleteUsedLocalFile(flac_dir)
+#         deleteUsedGCSFile(gcs_fileName)
+        
+#     except Exception as e:
+#         print(str(e))
+#         return HttpResponse(f"error :{e}")
+    
+#     return JsonResponse(result)
+
+
 
 def videoToAudio(folder_dir,video_file_name):
     
@@ -76,6 +102,8 @@ def videoToAudio(folder_dir,video_file_name):
         print("error : "+str(e))
     return flac_dir
 
+
+
 def upload_file_to_gcs(local_file_path, outputFileName):
 # 구글 클라우드 스토리지(gcs)에 오디오파일(flac) 업로드
     bucket_name = 'audio_storage_tpj'
@@ -86,6 +114,8 @@ def upload_file_to_gcs(local_file_path, outputFileName):
     print(blob.name)
     
     return blob.name
+
+
 
 def transcribe_audio(flacInGCS):
     
@@ -103,29 +133,28 @@ def transcribe_audio(flacInGCS):
     
     operation = client.long_running_recognize(config=config, audio=audio)
     print("Waiting for operation to complete")
-    response = operation.result(timeout=600)
-    
-    response_json = MessageToJson(response._pb)
-    jo = json.loads(response_json)
+    response = operation.result()
     
     transcript = ""
     words = []
-    
-    for j in jo.get("results"):
-        transcript += j.get("alternatives")[0].get("transcript")
-        words += j.get("alternatives")[0].get("words")
+
+    for result in response.results:
+        transcript += result.alternatives[0].transcript
+        words += result.alternatives[0].words
+
+    tandw = []
+
+    for w in words:
+        word = {}
+        word['word'] = w.word
+        word['start'] = w.start_time.seconds
+        tandw.append(word)
         
-    top_words = get_top_words(transcript)
     
-    results={
-        "transcript":transcript,
-        "words":words,
-        "top_words":top_words,
-    }
+    topWords = get_top_words(transcript)
     
-    final_response = json.dumps(results)
+    return {"transcript": transcript, "words":tandw, "topWords":topWords}
     
-    return final_response
 
 
 def get_top_words(transcript):
@@ -139,11 +168,22 @@ def get_top_words(transcript):
     return top_words
 
 
+
+def deleteUsedLocalFiles(audio_file_path,audio_paths):
+    print("deleteUsedLocalFile")
+    print(audio_file_path)
+    os.remove(audio_file_path)
+    for audio_path in audio_paths:
+        os.remove(audio_path)
+        
+
 def deleteUsedLocalFile(audio_file_path):
     print("deleteUsedLocalFile")
     print(audio_file_path)
     os.remove(audio_file_path)
     
+    
+   
    
 def deleteUsedGCSFile(gcs_fileName):
     bucket_name = 'audio_storage_tpj'
@@ -153,11 +193,12 @@ def deleteUsedGCSFile(gcs_fileName):
     blob.delete()
     print(gcs_fileName+"deleted.") 
     
+    
 
-
+@csrf_exempt
 def summaryToSpring(request):
     try:
-        transcript = request.GET.get('transcript')
+        transcript = request.POST.get('transcript')
         print("")
         summary = summarize_transcript(transcript)
         
@@ -166,7 +207,8 @@ def summaryToSpring(request):
         print(str(e))
         return HttpResponse(f"error :{e}")
     
-    return HttpResponse(summary)
+    return JsonResponse({"summary": summary})
+        
         
         
 def summarize_transcript(transcript):
@@ -174,15 +216,129 @@ def summarize_transcript(transcript):
     
     tokenizer = PreTrainedTokenizerFast.from_pretrained('digit82/kobart-summarization')
     model = BartForConditionalGeneration.from_pretrained('digit82/kobart-summarization')
-
-    raw_input_ids = tokenizer.encode(text)
-    input_ids = [tokenizer.bos_token_id] + raw_input_ids + [tokenizer.eos_token_id]
-
-    summary_ids = model.generate(torch.tensor([input_ids]),  num_beams=4,  max_length=512,  eos_token_id=1)
-    result = tokenizer.decode(summary_ids.squeeze().tolist(), skip_special_tokens=True)
- 
-    return result
+    result = ""
     
+    print(len(text))
+    if(len(text)>=1000):
+        print("looooooooooooong sentence")
+        kkma = Kkma()
+        sentences = kkma.sentences(text)
+        
+        sentence = ["".join(sentences[i:i+10]) for i in range(0, len(sentences), 10)]
+        len(sentence)
+        
+        
+        for s in sentence:
+            raw_input_ids = tokenizer.encode(s)
+            input_ids = [tokenizer.bos_token_id] + raw_input_ids + [tokenizer.eos_token_id]
+
+            summary_ids = model.generate(torch.tensor([input_ids]),  num_beams=1,  max_length=256,  eos_token_id=1)
+            result += tokenizer.decode(summary_ids.squeeze().tolist(), skip_special_tokens=True)
+ 
+
+    else:
+        raw_input_ids = tokenizer.encode(text)
+        input_ids = [tokenizer.bos_token_id] + raw_input_ids + [tokenizer.eos_token_id]
+
+        summary_ids = model.generate(torch.tensor([input_ids]),  num_beams=2,  max_length=1024,  eos_token_id=1)
+        result += tokenizer.decode(summary_ids.squeeze().tolist(), skip_special_tokens=True)
+    
+    return result
+
+    
+    
+def split_flac(input_flac, output_dir, chunk_duration_sec):
+    print("split_flac")
+    os.makedirs(output_dir, exist_ok=True)
+
+    input_basename = os.path.basename(input_flac)
+    output_prefix = output_dir + '/' + os.path.splitext(input_basename)[0]
+
+    # 청크 크기 (초)
+    chunk_size = int(chunk_duration_sec * 1000)
+
+    audio_paths=[]
+    # 청크로 음성 파일을 분할
+    audio = AudioSegment.from_file(input_flac, format="flac")
+    for i, chunk in enumerate(audio[::chunk_size]):
+        output_file = f"{output_prefix}_{i:03d}.flac"
+        chunk.export(output_file, format="flac")    
+        audio_paths.append(output_file)
+    
+    return audio_paths
+
+
+    
+def transcribe_audio_part(client,audio_path):
+
+    with open(audio_path, "rb") as audio_file:
+        content = audio_file.read()
+    audio = speech.RecognitionAudio(content=content)
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
+        sample_rate_hertz=44100,
+        language_code="ko-KR",
+        enable_word_time_offsets=True,
+        audio_channel_count=2,
+    )
+
+    response = client.recognize(config=config, audio=audio)
+    
+    transcript = ""
+    words = []
+    
+    for result in response.results:
+        transcript = result.alternatives[0].transcript
+        words = result.alternatives[0].words
+    
+    tandw = []
+        
+    for w in words:
+        word = {}
+        word['word'] = w.word
+        word['start'] = w.start_time.seconds
+        tandw.append(word)
+    
+    return {"audio_path": audio_path, "transcript": transcript, "words":tandw}      
+        
+        
+
+def transcribe_audio_parallel(audio_paths):
+
+    client = speech.SpeechClient()
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(transcribe_audio_part, client, audio_path) for audio_path in audio_paths]
+        
+        results = []
+        # 모든 작업이 완료될 때까지 기다립니다.
+        for future in concurrent.futures.as_completed(futures):
+            try:
+                result = future.result()
+                results.append(result)
+            except Exception as e:
+                print(f"Error: {e}")  
+
+        sorted_results = sorted(results,key=lambda x: x["audio_path"])
+
+        transcript = ""
+        wandt = []
+        
+        for index,result in enumerate(sorted_results):
+            timeCompensation = index*30
+            for w in result['words']:
+                w['start'] += timeCompensation
+            
+            transcript += " "
+            transcript += result['transcript']
+            wandt += result['words']
+            topWords = get_top_words(transcript)    
+
+    return {"transcript":transcript, "words":wandt, "topWords":topWords}
+    
+
+
+
     
     
     
